935121cadf0590e21867af2d90e9d68977411c88
shonada <vovaborkovskij@gmail.com>
2023-02-11 13:04:06 +0700

    lol

---
+++ AvaloniaApplication1/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvaloniaApplication1.Models;
+
+public partial class Client
+{
+    public int ClientId { get; set; }
+
+    public string Fio { get; set; } = null!;
+
+    public string Passport { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public virtual ICollection<Purchase> Purchases { get; } = new List<Purchase>();
+}
---
+++ AvaloniaApplication1/Models/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvaloniaApplication1.Models;
+
+public partial class Purchase
+{
+    public int PurchaseId { get; set; }
+
+    public int ProductIdFk { get; set; }
+
+    public int ClientIdFk { get; set; }
+
+    public DateOnly DatePurchase { get; set; }
+
+    public int TypePurchaseIdFk { get; set; }
+
+    public virtual Client ClientIdFkNavigation { get; set; } = null!;
+
+    public virtual Product ProductIdFkNavigation { get; set; } = null!;
+
+    public virtual TypesPurchase TypePurchaseIdFkNavigation { get; set; } = null!;
+}
